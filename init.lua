8bdc0a1179be6ad7478ffd8ca2f7d92450cffa00
Alexandru Ene <alex.ene0x11@gmail.com>
2025-06-07 12:09:35 +0100

    Fork it

--- init.lua
+++ init.lua
@@ -2,3 +2,59 @@
 require("config.lazy")
 
 vim.cmd("colorscheme catppuccin")
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- Command key mappings for macOS
+    vim.keymap.set('i', '<D-z>', '<Esc>ua', { desc = 'Undo in insert mode' })
+    -- vim.keymap.set('v', '<D-c>', '"+y', { desc = 'Copy in visual mode', silent = true })
+    vim.keymap.set('n', '<D-z>', 'u', { desc = 'Undo in normal mode' })
+    vim.keymap.set('n', '<D-v>', '"+p', { desc = 'Paste in normal mode' })
+    vim.keymap.set('v', '<D-v>', '"+p', { desc = 'Paste in visual mode' })
+    vim.keymap.set('i', '<D-v>', '<C-r>+', { desc = 'Paste in insert mode' })
+    vim.keymap.set('v', '<D-x>', '"+d', { desc = 'Cut in visual mode' })
+    
+    
+    -- Press Space+Space to open the smart file picker
+    vim.keymap.set('n', '<leader><space>', function() Snacks.picker.smart() end)
+
+    -- Press Space+/ to open the grep picker
+    vim.keymap.set('n', '<leader>/', function() Snacks.picker.grep() end)
+
+    -- Press Space+f+f to find files
+    vim.keymap.set('n', '<leader>ff', function() Snacks.picker.files() end)
+
+  end,
+})
+
+vim.api.nvim_create_autocmd('VimEnter', {
+  callback = function()
+    -- if vim.fn.has('mac') == 1 then
+      vim.cmd([[
+        vnoremap <D-c> "+y
+        vnoremap <D-x> "+d
+        nnoremap <D-v> "+p
+        inoremap <D-v> <C-r>+
+      ]])
+    -- end
+  end,
+})
+
+
+vim.keymap.set({'n', 'i', 'v'}, '<D-s>', function()
+    vim.cmd('write')
+    if vim.fn.mode() == 'i' then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>a', true, false, true), 'n', true)
+    end
+end, {
+    desc = 'Save file (Cmd+S)'
+})
+
+vim.keymap.set({'n', 'i', 'v'}, '<C-s>', function()
+    vim.cmd('write')
+    if vim.fn.mode() == 'i' then
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>a', true, false, true), 'n', true)
+    end
+end, {
+    desc = 'Save file (Cmd+S)'
+})
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,10 +14,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.lazyvim_check_order = false
+
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { import = "../lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins" },
   },
